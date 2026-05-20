ac902edf967d6241365bb28f38f1ade53cc12a21
Gabriel Nobre <gabrielsntnobre.dev@gmail.com>
2026-05-20 16:09:01 -0300

    Changes on the menu options

--- jc.sh
+++ jc.sh
@@ -2,21 +2,26 @@
 
 SRC_DIR="src"
 OUT_DIR="bin"
-MAIN_FILE=$(grep -rl "public static void main" "$SRC_DIR")
-MAIN_CLASS=$(echo "$MAIN_FILE" | sed "s|$SRC_DIR/||" | sed 's|/|.|g' | sed 's|.java||')
 
+compile(){
+	MAIN_FILE=$(grep -rl "public static void main" "$SRC_DIR")
+	MAIN_CLASS=$(echo "$MAIN_FILE" | sed "s|$SRC_DIR/||" | sed 's|/|.|g' | sed 's|.java||')
+
+	echo "Compiling..."
+	mkdir -p "$OUT_DIR"
+	javac -d "$OUT_DIR" $(find "$SRC_DIR" -name "*.java")
+}
 
 case "$1" in 
-	build)
-		echo "Compiling..."
-		mkdir -p "$OUT_DIR"
-		javac -d "$OUT_DIR" $(find "$SRC_DIR" -name "*.java")
+	build | -b)
+		compile
 		;;
-	run)
+	run | -r)
 		java -cp "$OUT_DIR" "$MAIN_CLASS"
 		;;
-	help)
-		echo "Commands: help, run"
+	help | -h)
+		echo "Java Compilation Utility"
+		echo "Commands:\nhelp	| -h\nrun	| -r\nbuild	| -b"
 		;;
 	*)
 		echo "Unknown argument"
